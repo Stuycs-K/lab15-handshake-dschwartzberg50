3157cf379fc0b6e0ba64c05564b620332c007d29
David Schwartzberg <dschwartzberg50@stuy.edu>
2025-01-07 02:31:59 -0500

    simple test for simple pipe network

--- basic_client.c
+++ basic_client.c
@@ -1,10 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include "pipe_networking.h"
 
 int main() {
-  int to_server;
-  int from_server;
-
-  from_server = client_handshake( &to_server );
+	int to_server, from_server;
+	from_server = client_handshake( &to_server );
+	printf("client completed handshake\n");
 	
-	printf("basic_client completed handshake\n");
+	int message;
+	if (read(from_server, &message, sizeof(message)) == -1) {
+		error("client: read from server");
+		exit(1);
+	}
+	
+	printf("(client) message is: %d\n", message);
+	
 }
--- basic_server.c
+++ basic_server.c
@@ -1,10 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include "pipe_networking.h"
 
 int main() {
-  int to_client;
-  int from_client;
-
+	int to_client, from_client;
   from_client = server_handshake( &to_client );
+	printf("server completed handshake\n");
 	
-	printf("basic_server completed handshake\n");
+	int message = 12345;
+	if (write(to_client, &message, sizeof(message)) == -1) {
+		error("server: write to client");
+		exit(1);
+	}
+	
 }
